37ede0a6192cc530ae67d63f1c0142c2d78caa6b
carollaynerosa <161317778+carollaynerosa@users.noreply.github.com>
2025-04-10 21:02:12 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -17,5 +17,25 @@ const diminuirFontBotao=document.getElementById('diminuir-fonte');
 
 const alternarContraste=document.getElementById('alterna-constrante');
 
+let tamanhoAtualFonte = 1;
+
+aumentaFontBotao.addEvertLintener('clich', function(){
+    tamanhoAtualFonte += 0.1;
+    document.body.style.fonSize=`${tamanhoAtualFonte}rem` 
+})
+
+diminuirFontBotao.addEvertLintener('clich', function(){
+    tamanhoAtualFonte -= 0.1;
+    document.body.style.fonSize=`${tamanhoAtualFonte}rem` 
+})
+
+alternarContraste.addEvertLintener('click', function (){
+    document.body.classLint.toggle('alto-contraste')
+})
 
 })
+
+scrollReveal().reveal('#inicio', {delay: 500})
+scrollReveal().reveal('#tropicalia', {delay: 500})
+scrollReveal().reveal('#galeria', {delay: 500})
+scrollReveal().reveal('#contato', {delay: 500})
